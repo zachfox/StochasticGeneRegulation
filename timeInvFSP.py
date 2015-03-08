9e63bee4b9a10fd966741fd8c32f9d8c71c92008
zachfox <zrussellfox@gmail.com>
2015-03-08 17:04:26 -0600

    added and removed commenting

--- timeInvFSP.py
+++ timeInvFSP.py
@@ -15,6 +15,11 @@ class easyFSP:
     '''
     
     def __init__(self,numSpecies, A_Matrix,errorTolerance,initialState,finalTime,initialTime):
+        ################################################################
+        #The state transition matrix (A matrix) is expected to be a 2D
+        #numpy array. As of now, error tolerance yield binary response, and
+        #the A matrix must be resized manually. 
+        ################################################################
         self.numSpecies = numSpecies
         self.A = A_Matrix
         self.tol = errorTolerance
@@ -24,18 +29,19 @@ class easyFSP:
         
         
     def FSPCalc(self):
-        '''Computes the Finite State Projection for time independent systems
-        within given error.
-        A is the state reaction matrix, tf is the final time of the projections, 
-        X_0 is the initial states, error is the amount of error between the FSP and 
-        the actual solution.    
-        '''
+        ################################################################            
+        #Computes the Finite State Projection for time independent systems
+        #within given error.
+        #A is the state reaction matrix, tf is the final time of the projections, 
+        #X_0 is the initial states, error is the amount of error between the FSP and 
+        #the actual solution.    
+        ################################################################
+        
         ones = np.array([(np.ones(np.shape(self.A[1])))])   #computes a 2D row of 1's.
-        matexp = linalg.expm(self.A*self.tf)                     #' ' matrix exponential
+        matexp = linalg.expm(self.A*self.tf)                #compute matrix exponential
         G = np.dot((np.dot(ones,matexp)),self.x0) 
         if G >= 1-self.tol:                 
             xf= np.dot(matexp,self.x0)       
-            #print X_f
             return xf
         else: 
             print('FSP is a no go, sire')        
